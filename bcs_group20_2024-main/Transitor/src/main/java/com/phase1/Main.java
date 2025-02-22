c003c06be67c289985addb4e267d8338a02413bb
adrianebien54 <adriane.bien54@gmail.com>
2025-02-22 16:57:49 +0100

    Initial commit from GitLab

---
+++ bcs_group20_2024-main/Transitor/src/main/java/com/phase1/Main.java
@@ -0,0 +1,24 @@
+package com.phase1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+//test commit
